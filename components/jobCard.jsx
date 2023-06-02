0c569c61022a0620f236aa513c6571a5e93b141a
Toheeb Ashorobi <tasho646fr@gmail.com>
2023-06-02 17:37:49 +0200

    JobCard done

---
+++ components/jobCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./JobCard.css";
+import { Link } from "react-router-dom";
+
+function JobCard() {
+  return (
+    <div id="main-box">
+      <div id="job-container">
+        <h1>hello everyone</h1>
+      </div>
+    </div>
+  );
+}
+
+export default JobCard;
---
+++ pages/JobSeekerPage.jsx
@@ -0,0 +1,2 @@
+// import React from "react";
+// import JobCard from "../components/JobCard";
