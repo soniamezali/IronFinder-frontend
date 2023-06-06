8adb9f77c077913b63c88bd1ff976d3ee874d505
Sonia Mezali <sonia.mezali@hotmail.fr>
2023-06-06 19:54:23 +0200

    changes

--- components/navBarJobSeeker.jsx
+++ components/navBarJobSeeker.jsx
@@ -6,7 +6,7 @@ function NavbarJobSeekerPage() {
     <>
       <nav>
         <div className="layout-banner">
-          <Link to="/">
+          <Link to="/job-seeker-homepage">
             <img className="home-logo" src="./../public/img/accueil.png"></img>
           </Link>
           <Link to="/">
@@ -17,7 +17,7 @@ function NavbarJobSeekerPage() {
           <Link to="/Log-Out">
             <p className="log-out">LOG OUT</p>
           </Link>
-          <Link to="/Job-Seeker/Profile">
+          <Link to="/job-seeker-profile">
             <p className="my-profile-link">My Profile</p>
           </Link>
         </div>
