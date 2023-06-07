b1665bdac6d9c8aefcdad739c0771ede8165aa3f
Sonia Mezali <sonia.mezali@hotmail.fr>
2023-06-07 13:08:24 +0200

    profiles

--- pages/JobSeekerDetailOffer.jsx
+++ pages/JobSeekerDetailOffer.jsx
@@ -1,21 +1,35 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
 import NavbarJobSeeker from "../components/NavBarJobSeeker";
 import Footer from "../components/footer";
-import axios from "axios";
+import service from "../service/api";
+
+import { AuthContext } from "../src/context/authContext"; // Importing AuthContext
 
 function JobSeekerDetailOffer() {
   const dialog = useRef();
   const { id } = useParams();
   const [oneJobOffer, setOneJobOffer] = useState(null);
+  const [jobOffers, setJobOffers] = useState([]); // State for job offers
+  const { jobOffer } = useContext(AuthContext); // Accessing jobOffer value from AuthContext
 
   useEffect(() => {
-    axios.get().then((response) => {
+    // Fetch the job offer details from the API
+    service.get(`/job-offer/${jobOffer._id}`).then((response) => {
       setOneJobOffer(response.data);
     });
   }, [id]);
 
   if (!oneJobOffer) return <div>Loading job offer...</div>;
+
+  const handleAddToFavourite = (oneJobOffer) => {
+    setJobOffers([...jobOffers, oneJobOffer]); // Add the selected job offer to the jobOffers state
+  };
+
+  const addToFavourite = () => {
+    handleAddToFavourite(oneJobOffer); // Call the handleAddToFavourite function with the selected job offer
+    dialog.current.showModal(); // Show the popup dialog
+  };
 
   return (
     <>
@@ -31,25 +45,37 @@ function JobSeekerDetailOffer() {
           >
             X
           </button>
-          <h3 style={{ fontSize: "2.5rem" }}>Successfully Applied!</h3>
+          <h3 style={{ fontSize: "2.5rem" }}>Added to favourite!</h3>
           <button className="go-to-profile-button">
-            <Link to="/job-seeker-homepage">Go to protile page !</Link>
+            <Link to="/job-seeker/profile">Go to profile page!</Link>
           </button>
         </dialog>
       </div>
+
       <div>
-        <dialog ref={dialog} className="popup">
-          <button
-            className="close-button"
-            onClick={() => dialog.current.close()}
-          >
-            X
-          </button>
-          <h3 style={{ fontSize: "2.5rem" }}>Added to favourite!</h3>
-          <button className="go-to-profile-button">
-            <Link to="/job-seeker/profile"> Go to profile page !</Link>
-          </button>
-        </dialog>
+        <h2>Job Offer Details</h2>
+        <div className="job-offer-details">
+          <img src={oneJobOffer.companyLogo} alt={oneJobOffer.jobTitle} />
+          <h3>{oneJobOffer.companyName}</h3>
+          <h2>{oneJobOffer.jobTitle}</h2>
+          <p>
+            <span>Contract Type: </span>
+            <span className="details">{oneJobOffer.contractType}</span>
+          </p>
+          <p>
+            Location: <span className="details">{oneJobOffer.jobLocation}</span>{" "}
+          </p>
+          <p>
+            Description:{" "}
+            <span className="details">{oneJobOffer.jobDescription}</span>
+          </p>
+        </div>
+        <button
+          className="addtofavourite-button-details"
+          onClick={addToFavourite}
+        >
+          Add To Favourite
+        </button>
       </div>
 
       <Footer />
